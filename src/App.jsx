731a2350da67caca4b1ff7f61d003475b49b1d8f
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-15 15:18:47 +0700

    remake

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
-import { createContext } from "react";
-import { AppHeader, AppCalculator } from "./components";
-
-export const UserContext = createContext();
-
 const App = () => {
-  return (
-    // responsive background
-    <div className="flex justify-center">
-      <div className="flex w-max max-w-lg flex-col gap-y-4 bg-gray-100 p-4">
-        <AppHeader />
-        <AppProvider childern={<AppCalculator />} />
-      </div>
-    </div>
-  );
+  return <></>;
 };
 export default App;
-
-const AppProvider = ({ childern }) => {
-  return <UserContext.Provider>{childern}</UserContext.Provider>;
-};
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppHeader } from "./AppHeader";
-export { default as AppCalculator } from "./Calculator/AppCalculator";
-export { default as CalculatorButtonsTable } from "./Calculator/CalculatorButtonsTable";
-export { default as CalculatorScreen } from "./Calculator/CalculatorScreen";
