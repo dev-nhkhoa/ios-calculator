a04f8257363f5d527156932a92b1417defa6ff0f
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-15 22:24:46 +0700

    style: add useContext Provider

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,12 +1,24 @@
 import CaclulatorScreen from "./CalculatorScreen";
 import CalculatorTable from "./CalculatorTable";
+import { useState, createContext } from "react";
+
+export const UserContext = createContext();
 
 const Calculator = () => {
+  const [output, setOutput] = useState(0);
+
   return (
     <div className="flex justify-center">
       <div className="flex w-max max-w-lg flex-col gap-4 bg-[#1C1C1C] text-white">
-        <CaclulatorScreen outputText={"screen"} />
-        <CalculatorTable />
+        <CaclulatorScreen outputText={output} />
+        <UserContext.Provider
+          value={{
+            setOutput,
+            output,
+          }}
+        >
+          <CalculatorTable />
+        </UserContext.Provider>
       </div>
     </div>
   );
