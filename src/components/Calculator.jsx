93578ad70ad729da142f2f729caded3a3ee9e9ea
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-15 20:13:51 +0700

    style: add background & screen & numbers table

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,9 +3,11 @@ import CalculatorTable from "./CalculatorTable";
 
 const Calculator = () => {
   return (
-    <div>
-      <CaclulatorScreen outputText={"screen"} />
-      <CalculatorTable />
+    <div className="flex justify-center">
+      <div className="flex w-full max-w-lg flex-col bg-[#1C1C1C] text-white">
+        <CaclulatorScreen outputText={"screen"} />
+        <CalculatorTable />
+      </div>
     </div>
   );
 };
