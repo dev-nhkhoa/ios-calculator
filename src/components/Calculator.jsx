0515004d09e05973b2b3faeb458743a51bd5d5a3
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-15 20:54:48 +0700

    refactor: make screen to fit content

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,7 +4,7 @@ import CalculatorTable from "./CalculatorTable";
 const Calculator = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex w-full max-w-lg flex-col bg-[#1C1C1C] text-white">
+      <div className="flex w-max max-w-lg flex-col gap-4 bg-[#1C1C1C] text-white">
         <CaclulatorScreen outputText={"screen"} />
         <CalculatorTable />
       </div>
