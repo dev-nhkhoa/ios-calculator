d1b2dcdba6de8810cb0c85e29a6315fcf5e96a48
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-18 21:53:25 +0700

    style: center the calculator and add ToDo

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,12 +4,13 @@ import { useState, createContext } from "react";
 
 export const UserContext = createContext();
 
+// TODO: ADD ANIMATE WHEN CLICK NUMBERS BTN & MATHS BTN
 const Calculator = () => {
   const [output, setOutput] = useState(0);
 
   return (
-    <div className="flex justify-center">
-      <div className="flex w-max max-w-lg flex-col gap-4 bg-[#1C1C1C] text-white">
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex w-max max-w-lg flex-col gap-4 rounded-lg bg-[#1C1C1C] p-[10px] text-white">
         <CaclulatorScreen outputText={output} />
         <UserContext.Provider
           value={{
