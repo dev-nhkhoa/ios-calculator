e2e28ee855277e2ead5051f94e9bf9e720cffb73
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-18 22:06:27 +0700

    chore: add metadata

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+document.title = "IOS Calculator";
+
+const metaDescription = document.createElement("meta");
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.href = "/favicon.ico";
+metaDescription.name = "My Pratice React Code: IOS Calculator";
+metaDescription.content = "Made by Damocloids";
+document.head.appendChild(metaDescription);
+document.head.appendChild(favicon);
 root.render(
   <React.StrictMode>
     <App />
